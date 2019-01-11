284e6978dd5376a07afdc99f7296fc67d0800b17
Антон Жлуктарёв <kilpatricktheraptor@gmail.com>
2019-01-11 18:09:00 +0200

    Created methods to change subgroup and to get subgroup

--- src/main/java/com/sunforge/db/UserOperations.java
+++ src/main/java/com/sunforge/db/UserOperations.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserOperations {
@@ -12,15 +13,43 @@ public class UserOperations {
     private static final Logger logger = LogManager.getLogger(UserOperations.class);
 
     public static void initializeUser(long telegramId, String username, String firstName, String lastName) throws SQLException{
-        final String queryToInitializeUser = "INSERT INTO users (telegram_id, username, first_name, last_name)" +
+        final String query = "REPLACE INTO users (telegram_id, username, first_name, last_name)" +
                 " VALUES ("+telegramId+", '" +username + "', '" + firstName + "', '" + lastName + "')";
-        logger.trace("Trying to execute: " + queryToInitializeUser);
+        logger.debug("Trying to execute: " + query);
 
         try(Connection con = DataSource.getConnection();
-            PreparedStatement pst = con.prepareStatement(queryToInitializeUser);
+            PreparedStatement pst = con.prepareStatement(query)
         ){
             pst.execute();
-            logger.debug("Successfully inserted new user: [" + telegramId + "," + username + "," + firstName + "," + lastName);
+            logger.debug("Successfully inserted new user: [" + telegramId + "," + username + "," + firstName + "," + lastName+"]");
+        }
+    }
+
+    public static void changeSubgroup(long telegramId, int chosenSubgroup) throws SQLException{
+        final String query = "UPDATE users SET subgroup=" + chosenSubgroup + " WHERE telegram_id="+telegramId;
+        logger.debug("Trying to execute: " + query);
+
+        try(Connection con = DataSource.getConnection();
+            PreparedStatement pst = con.prepareStatement(query)
+        ){
+            pst.execute();
+            logger.debug("Successfully changed subgroup of user " + telegramId +" to " + chosenSubgroup);
+        }
+    }
+
+    public static short getSubgroup(long telegramId) throws SQLException{
+        final String query = "SELECT subgroup FROM users WHERE telegram_id=" + telegramId;
+        logger.debug("Trying to execute: " + query);
+
+        try(Connection con = DataSource.getConnection();
+            PreparedStatement pst = con.prepareStatement(query);
+            ResultSet rs = pst.executeQuery()
+        ){
+            rs.next();
+            short redeemedSubgroup = rs.getShort("subgroup");
+            logger.debug("Successfully redeemed subgroup of user " + telegramId +": " + redeemedSubgroup);
+
+            return redeemedSubgroup;
         }
     }
 }
