25c9c48891df2b379c88b4a0cc8a518fb864d8dc
Антон Жлуктарёв <kilpatricktheraptor@gmail.com>
2019-01-13 17:03:12 +0200

    Polished ChangeSubgroupCallback

--- src/main/java/com/sunforge/callbacks/ChangeSubgroupCallback.java
+++ src/main/java/com/sunforge/callbacks/ChangeSubgroupCallback.java
@@ -11,19 +11,20 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.sql.SQLException;
 
-public class ChangeSubgroupCallback {
+class ChangeSubgroupCallback {
 
     private static final Logger logger = LogManager.getLogger(ChangeSubgroupCallback.class);
     private static final LocalizationBundle localizationBundle = LocalizationBundle.getInstance();
 
-    public static void changeSubgroupCallback(Update passedUpdate, int chosenSubgroup){
+    static void changeSubgroupCallback(Update passedUpdate, int chosenSubgroup) {
         long chat_id = passedUpdate.getCallbackQuery().getMessage().getChatId();
         try {
             UserOperations.changeSubgroup(chat_id, chosenSubgroup);
         } catch (SQLException e) {
             logger.error("Couldn't get the subgroup of " + chat_id, e);
-            SendMessage snd = new SendMessage().setChatId(passedUpdate.getCallbackQuery().getMessage().getChatId())
-                                .setText(localizationBundle.getString(LocalizationField.ERROR_DB_RETRIEVE_SUBGROUP));
+            SendMessage snd = new SendMessage()
+                    .setChatId(passedUpdate.getCallbackQuery().getMessage().getChatId())
+                    .setText(localizationBundle.getString(LocalizationField.ERROR_DB_RETRIEVE_SUBGROUP));
             UniScheduleBot.getInstance().sendMessage(snd);
         }
     }
