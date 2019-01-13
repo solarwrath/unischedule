b82f2770ace4141a3540fac95ccd256c628e0cb1
Антон Жлуктарёв <kilpatricktheraptor@gmail.com>
2019-01-13 17:08:44 +0200

    Polished ScheduleNextWeekMenuCallback

--- src/main/java/com/sunforge/callbacks/ScheduleNextWeekMenuCallback.java
+++ src/main/java/com/sunforge/callbacks/ScheduleNextWeekMenuCallback.java
@@ -17,22 +17,27 @@ public class ScheduleNextWeekMenuCallback {
     private static final LocalizationBundle localizationBundle = LocalizationBundle.getInstance();
     private static final Logger logger = LogManager.getLogger(ScheduleNextWeekMenuCallback.class);
 
-    public static void changeKeyboardToDays(Update passedUpdate) {
-        EditMessageText editMessageText = new EditMessageText();
-        editMessageText.setChatId(passedUpdate.getCallbackQuery().getMessage().getChatId())
-        .setMessageId(passedUpdate.getCallbackQuery().getMessage().getMessageId())
-        .setText(localizationBundle.getString(LocalizationField.SCHEDULE_RESPONSE));
+    static void changeKeyboardToDays(Update passedUpdate) {
+        EditMessageText editMessageText = new EditMessageText()
+                                        .setChatId(passedUpdate.getCallbackQuery().getMessage().getChatId())
+                                        .setMessageId(passedUpdate.getCallbackQuery().getMessage().getMessageId())
+                                        .setText(localizationBundle.getString(LocalizationField.SCHEDULE_RESPONSE));
 
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
 
+        //Keyboard with 4 days:
+        //Mon Tues Wed Fri
         List<InlineKeyboardButton> daysRow = new ArrayList<>();
+
         daysRow.add(new InlineKeyboardButton().setText(localizationBundle.getString(LocalizationField.MONDAY_SHORTENED)).setCallbackData("schedule_next_week_monday"));
         daysRow.add(new InlineKeyboardButton().setText(localizationBundle.getString(LocalizationField.TUESDAY_SHORTENED)).setCallbackData("schedule_next_week_tuesday"));
         daysRow.add(new InlineKeyboardButton().setText(localizationBundle.getString(LocalizationField.WEDNESDAY_SHORTENED)).setCallbackData("schedule_next_week_wednesday"));
         daysRow.add(new InlineKeyboardButton().setText(localizationBundle.getString(LocalizationField.FRIDAY_SHORTENED)).setCallbackData("schedule_next_week_friday"));
 
+        //Adding Back button
         List<InlineKeyboardButton> backButtonRow = new ArrayList<>();
+
         backButtonRow.add(new InlineKeyboardButton().setText(localizationBundle.getString(LocalizationField.BACK)).setCallbackData("schedule_menu"));
 
         rows.add(daysRow);
